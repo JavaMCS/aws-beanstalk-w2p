25cc7de6d08113556754280ddf6d5ae48bce49f5
javamcs <javamcs@gmail.com>
2015-04-08 12:35:38 -0300

    Add RDS Connect (PARAMs,db)

--- applications/welcome/models/db.py
+++ applications/welcome/models/db.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 #########################################################################
 ## This scaffolding model makes your app work on Google App Engine too
 ## File is released under public domain and you can use without limitations
@@ -14,15 +16,22 @@ from gluon.contrib.appconfig import AppConfig
 ## once in production, remove reload=True to gain full speed
 myconf = AppConfig(reload=True)
 
+DB_DBMS = os.environ['DB_DBMS']
+DB_HOST = os.environ['DB_HOST']
+DB_NAME = os.environ['DB_NAME']
+DB_USER = os.environ['DB_USER']
+DB_PASSWORD = os.environ['DB_PASSWORD']
 
-if not request.env.web2py_runtime_gae:
+db = DAL('%s://%s:%s@%s/%s'%(DB_DBMS,DB_USER,DB_PASSWORD,DB_HOST,DB_NAME), pool_size=0)
+
+#if not request.env.web2py_runtime_gae:
     ## if NOT running on Google App Engine use SQLite or other DB
-    db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
-else:
+#    db = DAL(myconf.take('db.uri'), pool_size=myconf.take('db.pool_size', cast=int), check_reserved=['all'])
+#else:
     ## connect to Google BigTable (optional 'google:datastore://namespace')
-    db = DAL('google:datastore+ndb')
+#    db = DAL('google:datastore+ndb')
     ## store sessions and tickets there
-    session.connect(request, response, db=db)
+#    session.connect(request, response, db=db)
     ## or store session in Memcache, Redis, etc.
     ## from gluon.contrib.memdb import MEMDB
     ## from google.appengine.api.memcache import Client
